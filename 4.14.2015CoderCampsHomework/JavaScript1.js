1668d7c88c5d2cc003c7e4b2b346b0f74375154d
ChucklesStern <Chuckstern@mac.com>
2015-04-15 09:51:20 -0700

    initial commit

---
+++ 4.14.2015CoderCampsHomework/JavaScript1.js
@@ -0,0 +1,54 @@
+﻿var FirebaseURL = 'https://radiant-torch-6001.firebaseio.com/.json';
+
+var AddressBook = function (name, phone, address, email) {
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.email = email;
+}
+
+var contacts = [];
+
+var createAddress = function () {
+    var name = document.getElementById('inputName').value;
+    var phone = document.getElementById('inputPhone').value;
+    var address = document.getElementById('inputAddress').value;
+    var email = document.getElementById('inputEmail').value;
+    var contactObj = new AddressBook(name, phone, address, email);
+    PostAjax(contactObj);
+}
+
+var PostAjax = function (contactPerson) {
+
+    var request = new XMLHttpRequest();
+
+    request.open('POST', FirebaseURL, true);
+
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            contacts.push(contactPerson)
+            console.log("Sucsess");
+        } else {
+            console.log("You have a" + this.status + "code");
+        }
+    }
+    request.send(JSON.stringify(contactPerson));
+    console.log(JSON.stringify(contactPerson));
+
+}
+
+var GetAjax = function (contacts) {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', FirebaseURL, true);
+
+    request.onload = function () {
+        var response = JSON.parse(this.response);
+        for (var prop in response) {
+            console.log(prop);
+            console.log(response[prop]);
+        }
+    }
+    request.send()
+}
+GetAjax();
---
+++ 4.14.2015CoderCampsHomework/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_4._14._2015CoderCampsHomework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_4._14._2015CoderCampsHomework")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35485f02-519d-46a2-9a9f-d29a8984023e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
